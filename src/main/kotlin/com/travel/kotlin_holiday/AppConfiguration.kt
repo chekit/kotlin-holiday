298630753ebd94d88a06438a5e6457d3f3000b69
Anton Cherepov <anton.cherepov@rabobank.nl>
2025-01-12 18:17:27 +0100

    finish lab 1b

--- src/main/kotlin/com/travel/kotlin_holiday/AppConfiguration.kt
+++ src/main/kotlin/com/travel/kotlin_holiday/AppConfiguration.kt
@@ -1,26 +1,31 @@
 package com.travel.kotlin_holiday
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.travel.kotlin_holiday.data.DefaultHolidaysRepository
 import com.travel.kotlin_holiday.data.FsHolidaysRepository
 import com.travel.kotlin_holiday.data.HolidaysRepositoryBase
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
 @ConfigurationProperties(prefix = "app.holidays.persistence")
 data class AppProperties(val type: String, val file: String = "db.json");
+
 
 @Configuration
 class AppConfiguration() {
 
     @Bean
-    fun repositoryConfigurer(objectMapper: ObjectMapper, appProperties: AppProperties): HolidaysRepositoryBase {
-        return if (appProperties.type == "file") {
-            FsHolidaysRepository(objectMapper, appProperties.file)
-        } else {
-            DefaultHolidaysRepository();
-        }
+    @Profile("!demo")
+    fun fsRepositoryConfigurer(objectMapper: ObjectMapper, appProperties: AppProperties): HolidaysRepositoryBase {
+        return FsHolidaysRepository(objectMapper, appProperties.file);
+    }
+
+    @Bean
+    @Profile("demo")
+    fun defaultRepositoryConfigurer(): HolidaysRepositoryBase {
+        return DefaultHolidaysRepository();
     }
 }
